01af9930a263da3be3c3e603cca4a6f9d86ffab8
kosbornept <kwosborne.pt@gmail.com>
2022-09-16 17:55:55 +0100

    Patch 1.0
    Major Content Update
    Removed
    Off Canvas Nav Modal
    Responsive Design (to be added next update)
    Section Hide/Show feature
    
    Added
    One Page scroll design
    Nav Bar
    Animations between Sections
    Intersection JS for Section/Nav

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
 //init
-$("#aboutPage").show()
-$("#projectPage").hide()
-$("#contactPage").hide()
 $(".langEN").show()
 $(".langPT").hide()
 
-$("#langEnPicker").click(function(){
+// English Language Switch Function
+$("#langEnPicker").click(function () {
     $(".langEN").show()
     $(".langPT").hide()
 })
 
-$("#langPtPicker").click(function(){
+// Portuguese Language Switch Function
+$("#langPtPicker").click(function () {
     $(".langPT").show()
     $(".langEN").hide()
 })
 
-$(".homeAbout").click(function(){
-    $("#aboutPage").show()
-    $(".homeAbout").addClass("active")
-    $("#projectPage").hide()
-    $(".homeProject").removeClass("active")
-    $("#contactPage").hide()
-    $(".homeSkills").removeClass("active")
-})
-$(".homeProject").click(function(){
-    $("#projectPage").show()
-    $(".homeProject").addClass("active")
-    $("#aboutPage").hide()
-    $(".homeAbout").removeClass("active")
-    $("#contactPage").hide()
-    $(".homeSkills").removeClass("active")
-})
-$(".homeSkills").click(function(){
-    $("#contactPage").show()
-    $(".homeSkills").addClass("active")
-    $("#projectPage").hide()
-    $(".homeProject").removeClass("active")
-    $("#aboutPage").hide()
-    $(".homeAbout").removeClass("active")
-})
-
+// Dark Mode Toggle
 function darkMode() {
     var docuBody = document.body;
     docuBody.classList.toggle("dark-mode");
     var docuMenu = document.getElementById("offcanvasNavbar");
     docuMenu.classList.toggle("text-bg-dark");
- }
+}
+
+// Intersection Observer for Animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Intersection Observer for Active Nav Link
+const navObserver = new IntersectionObserver((entries, options) => {
+    var dropItemMenu = '.projectDrop'
+    $(dropItemMenu).removeClass("active")
+    entries.forEach((entry) => {
+        // console.log($(entry.target));
+        var navActive = $(entry.target).data('navbar-item');
+        if (entry.isIntersecting) {
+            $(navActive).addClass("active")
+            if (entry.target.classList.contains('dropCheck')) {
+                $(dropItemMenu).addClass("active")
+            }
+        } else {
+            $(navActive).removeClass("active")
+        }
+    });
+}, {
+    threshold: .7
+});
+const activeNav = document.querySelectorAll('[data-navbar-item]');
+activeNav.forEach((el) => navObserver.observe(el));
